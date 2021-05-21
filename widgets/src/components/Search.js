f34c2452ef01e2a2e4c0b3a4593c907981d27306
Vadim <nevsky118@gmail.com>
2021-05-21 22:10:53 +0300

    154 The useEffect Hook

---
+++ widgets/src/components/Search.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+
+const Search = () => {
+	const [term, setTerm] = useState('');
+
+	useEffect(() => {
+		console.log({ term });
+	}, [term]);
+
+	return (
+		<div>
+			<div className="ui form">
+				<div className="field">
+					<label>Enter Search Term</label>
+					<input
+						value={term}
+						onChange={e => setTerm(e.target.value)}
+						className="input"
+					/>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Search;
