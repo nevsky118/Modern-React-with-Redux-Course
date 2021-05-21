5dfa426f9fb0b7337ec3011867505bb2c9ed6d04
Vadim <nevsky118@gmail.com>
2021-05-21 22:51:53 +0300

    157 Executing the Request from useEffect

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,10 +1,23 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Search = () => {
 	const [term, setTerm] = useState('');
 
 	useEffect(() => {
-		console.log({ term });
+		const search = async () => {
+			await axios.get('https://en.wikipedia.org/w/api.php', {
+				params: {
+					action: 'query',
+					list: 'search',
+					origin: '*',
+					format: 'json',
+					srsearch: term,
+				},
+			});
+		};
+
+		search();
 	}, [term]);
 
 	return (
