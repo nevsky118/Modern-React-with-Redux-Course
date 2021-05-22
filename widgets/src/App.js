4bd199714d3b5af1aa2545b99a1da9024d66a4b0
Vadim <nevsky118@gmail.com>
2021-05-22 12:38:00 +0300

    176 Hiding and Showing the Option List

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Dropdown from './components/Dropdown';
 import Search from './components/Search';
@@ -34,9 +34,15 @@ const options = [
 ];
 
 export default () => {
+	const [selected, setSelected] = useState(options[0]);
+
 	return (
 		<div>
-			<Dropdown options={options} />
+			<Dropdown
+				onSelectedChange={setSelected}
+				selected={selected}
+				options={options}
+			/>
 		</div>
 	);
 };
