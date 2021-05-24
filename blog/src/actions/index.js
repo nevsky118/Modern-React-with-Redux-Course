be0c2c46d6fc44313415eaaebbbfbc2d890c0cfe
Vadim <nevsky118@gmail.com>
2021-05-24 15:05:02 +0300

    274 Fetching Singular Records

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -3,5 +3,11 @@ import jsonPlaceholder from '../apis/jsonPlaceholder';
 export const fetchPosts = () => async dispatch => {
 	const responce = await jsonPlaceholder.get('/posts');
 
-	dispatch({ type: 'FETCH_POSTS', payload: responce });
+	dispatch({ type: 'FETCH_POSTS', payload: responce.data });
+};
+
+export const fetchUser = id => async dispatch => {
+	const responce = await jsonPlaceholder.get(`/users/${id}`);
+
+	dispatch({ type: 'FETCH_USER', payload: responce.data });
 };
