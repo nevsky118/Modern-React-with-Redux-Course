bb9062ac7dd2dd890da94b405538326f019e280a
Vadim <nevsky118@gmail.com>
2021-05-30 11:43:14 +0300

    398 An App with Context

---
+++ translate/src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class App extends React.Component {
+	state = { language: 'english' };
+
+	render() {
+		return (
+			<div className="ui container">
+				<div>
+					Select a language:
+					<i className="flag us" />
+					<i className="flag ru" />
+				</div>
+			</div>
+		);
+	}
+}
+
+export default App;
